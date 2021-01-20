85adc8a28b6755e57f45f4557416bf2cd9672246
Ahmet Tekçe <ahmettekce@msn.com>
2021-01-20 14:42:07 +0300

    Proje dosyası ekle.

---
+++ ClassMetotDemo1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassMetotDemo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
